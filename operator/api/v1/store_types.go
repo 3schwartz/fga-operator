7bcb56e1302a491bd1f7a69519e8f7e078268498
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2024-05-17 13:12:50 +0200

    made crd store

--- operator/api/v1/store_types.go
+++ operator/api/v1/store_types.go
@@ -20,16 +20,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // StoreSpec defines the desired state of Store
 type StoreSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Store. Edit store_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Name of the store
+	Name string `json:"Name,omitempty"`
 }
 
 // StoreStatus defines the observed state of Store
