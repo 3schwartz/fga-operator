d4e565f1793ec2c532275f396423b27cce195d81
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2024-09-13 12:57:27 +0200

    using constant

--- operator/internal/configurations/env_test.go
+++ operator/internal/configurations/env_test.go
@@ -1,8 +1,9 @@
 package configurations
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -16,36 +17,35 @@ func newTestLogger() logr.Logger {
 }
 
 func TestGetRequeueAfterFromEnv(t *testing.T) {
-	// Define test cases with different environment values and expected outcomes
 	testCases := []struct {
 		envValue       string
 		expectedResult time.Duration
 		description    string
 	}{
 		// Test case with no environment variable set (default value should be used)
-		{"", 45 * time.Second, "REQUEUE_AFTER_DURATION not set, expect default value of 45 seconds"},
+		{"", 45 * time.Second, fmt.Sprintf("%s not set, expect default value of 45 seconds", RequeueAfterDuration)},
 
 		// Test case with valid second values
-		{"30s", 30 * time.Second, "REQUEUE_AFTER_DURATION set to 30 seconds"},
-		{"90s", 90 * time.Second, "REQUEUE_AFTER_DURATION set to 90 seconds"},
+		{"30s", 30 * time.Second, fmt.Sprintf("%s set to 30 seconds", RequeueAfterDuration)},
+		{"90s", 90 * time.Second, fmt.Sprintf("%s set to 90 seconds", RequeueAfterDuration)},
 
 		// Test case with valid minute values
-		{"2m", 2 * time.Minute, "REQUEUE_AFTER_DURATION set to 2 minutes"},
-		{"5m", 5 * time.Minute, "REQUEUE_AFTER_DURATION set to 5 minutes"},
+		{"2m", 2 * time.Minute, fmt.Sprintf("%s set to 2 minutes", RequeueAfterDuration)},
+		{"5m", 5 * time.Minute, fmt.Sprintf("%s set to 5 minutes", RequeueAfterDuration)},
 
 		// Test case with valid hour values
-		{"1h", 1 * time.Hour, "REQUEUE_AFTER_DURATION set to 1 hour"},
-		{"3h", 3 * time.Hour, "REQUEUE_AFTER_DURATION set to 3 hours"},
+		{"1h", 1 * time.Hour, fmt.Sprintf("%s set to 1 hour", RequeueAfterDuration)},
+		{"3h", 3 * time.Hour, fmt.Sprintf("%s set to 3 hours", RequeueAfterDuration)},
 
 		// Test case with invalid value (default should be used)
-		{"invalid-value", 45 * time.Second, "REQUEUE_AFTER set to invalid value, expect default value of 45 seconds"},
+		{"invalid-value", 45 * time.Second, fmt.Sprintf("%s set to invalid value, expect default value of 45 seconds", RequeueAfterDuration)},
 	}
 
 	// Iterate over each test case
 	for _, testCase := range testCases {
 		t.Run(testCase.description, func(t *testing.T) {
 			// Arrange
-			err := os.Setenv("REQUEUE_AFTER", testCase.envValue)
+			err := os.Setenv(RequeueAfterDuration, testCase.envValue)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -55,10 +55,12 @@ func TestGetRequeueAfterFromEnv(t *testing.T) {
 			duration := GetRequeueAfterFromEnv(logger)
 
 			// Assert
-			assert.Equal(t, testCase.expectedResult, duration, testCase.description)
+			if !reflect.DeepEqual(duration, testCase.expectedResult) {
+				t.Errorf("expected %v, got %v", testCase.expectedResult, duration)
+			}
 
 			// Clean up environment variable
-			err = os.Unsetenv("REQUEUE_AFTER")
+			err = os.Unsetenv(RequeueAfterDuration)
 			if err != nil {
 				t.Fatal(err)
 			}
