d1b0f1b819b773ffedfe23ce5608c960da526254
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2024-09-21 20:04:57 +0200

    created test for specific annotation

--- operator/internal/controller/authorizationmodel/authorizationmodel_controller_test.go
+++ operator/internal/controller/authorizationmodel/authorizationmodel_controller_test.go
@@ -41,70 +41,87 @@ func getLowercaseUUID() string {
 
 var _ = Describe("AuthorizationModel Controller", func() {
 	Context("When reconciling a resource", func() {
+		var storeId string
+		var name string
 
-		It("given auth model version label then update deployment to correct version", func() {
-			// TODO
-		})
-
-		It("given no auth model version label then update deployment to latest", func() {
-			// Arrange
-
-			storeId := getLowercaseUUID()
-			authModelId := getLowercaseUUID()
-			namespaceName := getLowercaseUUID()
-			name := getLowercaseUUID()
+		BeforeEach(func() {
+			name = getLowercaseUUID()
+			storeId = getLowercaseUUID()
 			namespace := &corev1.Namespace{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      namespaceName,
-					Namespace: namespaceName,
+					Name:      name,
+					Namespace: name,
 				},
 			}
-			deploymentName := getLowercaseUUID()
-			containerName := getLowercaseUUID()
 			Expect(k8sClient.Create(ctx, namespace)).To(Succeed())
-
 			store := extensionsv1.Store{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      name,
-					Namespace: namespaceName,
+					Namespace: name,
 				},
 				Spec: extensionsv1.StoreSpec{
 					Id: storeId,
 				},
 			}
 			Expect(k8sClient.Create(ctx, &store)).To(Succeed())
-			deployment := appsV1.Deployment{
+		})
+
+		It("given auth model version label then update deployment to correct version", func() {
+			// Arrange
+			authModelId := getLowercaseUUID()
+			deploymentName := getLowercaseUUID()
+			modelVersion := extensionsv1.ModelVersion{
+				Major: 0,
+				Minor: 0,
+				Patch: 1,
+			}
+
+			deployment := createDeploymentWithAnnotations(name, deploymentName, map[string]string{
+				extensionsv1.OpenFgaStoreLabel:            name,
+				extensionsv1.OpenFgaAuthModelVersionLabel: modelVersion.String(),
+			})
+			Expect(k8sClient.Create(ctx, &deployment)).To(Succeed())
+
+			authorizationModel := extensionsv1.AuthorizationModel{
 				ObjectMeta: metav1.ObjectMeta{
-					Namespace: namespaceName,
-					Name:      deploymentName,
-					Labels: map[string]string{
-						extensionsv1.OpenFgaStoreLabel: name,
-					},
+					Name:      name,
+					Namespace: name,
 				},
-				Spec: appsV1.DeploymentSpec{
-					Selector: &metav1.LabelSelector{
-						MatchLabels: map[string]string{"app": deploymentName},
-					},
-					Template: corev1.PodTemplateSpec{
-						ObjectMeta: metav1.ObjectMeta{Name: getLowercaseUUID(), Labels: map[string]string{"app": deploymentName}},
-						Spec: corev1.PodSpec{
-							Containers: []corev1.Container{
-								{
-									Image: "some-image",
-									Name:  getLowercaseUUID(),
-								},
-								{
-									Image: "some-othe-image",
-									Name:  containerName,
-									Env: []corev1.EnvVar{
-										{Name: extensionsv1.OpenFgaStoreIdEnv, Value: "store-id"},
-									},
-								},
+				Spec: extensionsv1.AuthorizationModelSpec{
+					Instances: []extensionsv1.AuthorizationModelInstance{
+						{
+							Id: getLowercaseUUID(),
+							Version: extensionsv1.ModelVersion{
+								Major: 1,
+								Minor: 2,
+								Patch: 3,
 							},
+							AuthorizationModel: getLowercaseUUID(),
+						},
+						{
+							Id:                 authModelId,
+							Version:            modelVersion,
+							AuthorizationModel: getLowercaseUUID(),
 						},
 					},
 				},
 			}
+
+			// Act
+			Expect(k8sClient.Create(ctx, &authorizationModel)).To(Succeed())
+
+			// Assert
+			validateDeployment(deploymentName, name, storeId, authModelId, modelVersion)
+		})
+
+		It("given no auth model version label then update deployment to latest", func() {
+			// Arrange
+			authModelId := getLowercaseUUID()
+			deploymentName := getLowercaseUUID()
+
+			deployment := createDeploymentWithAnnotations(name, deploymentName, map[string]string{
+				extensionsv1.OpenFgaStoreLabel: name,
+			})
 			Expect(k8sClient.Create(ctx, &deployment)).To(Succeed())
 
 			modelVersion := extensionsv1.ModelVersion{
@@ -115,7 +132,7 @@ var _ = Describe("AuthorizationModel Controller", func() {
 			authorizationModel := extensionsv1.AuthorizationModel{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      name,
-					Namespace: namespaceName,
+					Namespace: name,
 				},
 				Spec: extensionsv1.AuthorizationModelSpec{
 					Instances: []extensionsv1.AuthorizationModelInstance{
@@ -141,48 +158,89 @@ var _ = Describe("AuthorizationModel Controller", func() {
 			Expect(k8sClient.Create(ctx, &authorizationModel)).To(Succeed())
 
 			// Assert
-			Eventually(func() error {
-				deployment := &appsV1.Deployment{}
-				if err := k8sClient.Get(ctx, types.NamespacedName{
-					Name:      deploymentName,
-					Namespace: namespaceName,
-				}, deployment); err != nil {
-					return err
-				}
-
-				// Validate that the deployment has the environment variables in the container with name 'containerName'
-				foundStoreId := false
-				foundAuthModelId := false
-				for _, container := range deployment.Spec.Template.Spec.Containers {
-					if container.Name != containerName {
-						continue
-					}
-					for _, envVar := range container.Env {
-						if envVar.Name == extensionsv1.OpenFgaStoreIdEnv && envVar.Value == storeId {
-							foundStoreId = true
-						}
-						if envVar.Name == extensionsv1.OpenFgaAuthModelIdEnv && envVar.Value == authModelId {
-							foundAuthModelId = true
-						}
-					}
-					break // Exit loop once the correct container is found and checked
-				}
-
-				if !foundStoreId {
-					return fmt.Errorf("container %s does not have env var %s with value %s", containerName, extensionsv1.OpenFgaStoreIdEnv, storeId)
-				}
-				if !foundAuthModelId {
-					return fmt.Errorf("container %s does not have env var %s with value %s", containerName, extensionsv1.OpenFgaAuthModelIdEnv, authModelId)
-				}
-
-				// Validate that the deployment has the annotation OpenFgaAuthModel with value "1.2.3"
-				if deployment.Annotations[extensionsv1.OpenFgaAuthModelVersionLabel] != modelVersion.String() {
-					return fmt.Errorf("deployment does not have annotation %s with value %s", extensionsv1.OpenFgaAuthModelVersionLabel, modelVersion.String())
-				}
-
-				return nil
-			}, duration, interval).Should(Succeed())
-
+			validateDeployment(deploymentName, name, storeId, authModelId, modelVersion)
 		})
 	})
 })
+
+func createDeploymentWithAnnotations(namespaceName, deploymentName string, annotations map[string]string) appsV1.Deployment {
+	return appsV1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespaceName,
+			Name:      deploymentName,
+			Labels:    annotations,
+		},
+		Spec: appsV1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"app": deploymentName},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{Name: getLowercaseUUID(), Labels: map[string]string{"app": deploymentName}},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Image: getLowercaseUUID(),
+							Name:  getLowercaseUUID(),
+						},
+						{
+							Image: getLowercaseUUID(),
+							Name:  getLowercaseUUID(),
+							Env: []corev1.EnvVar{
+								{Name: extensionsv1.OpenFgaAuthModelIdEnv, Value: getLowercaseUUID()},
+							},
+						},
+						{
+							Image: getLowercaseUUID(),
+							Name:  getLowercaseUUID(),
+							Env: []corev1.EnvVar{
+								{Name: extensionsv1.OpenFgaStoreIdEnv, Value: getLowercaseUUID()},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func validateDeployment(deploymentName, namespaceName, storeId, authModelId string, modelVersion extensionsv1.ModelVersion) {
+	Eventually(func() error {
+		deployment := &appsV1.Deployment{}
+		if err := k8sClient.Get(ctx, types.NamespacedName{
+			Name:      deploymentName,
+			Namespace: namespaceName,
+		}, deployment); err != nil {
+			return err
+		}
+
+		// Validate that all containers have the necessary environment variables
+		for _, container := range deployment.Spec.Template.Spec.Containers {
+			foundStoreId := false
+			foundAuthModelId := false
+
+			// Check environment variables in each container
+			for _, envVar := range container.Env {
+				if envVar.Name == extensionsv1.OpenFgaStoreIdEnv && envVar.Value == storeId {
+					foundStoreId = true
+				}
+				if envVar.Name == extensionsv1.OpenFgaAuthModelIdEnv && envVar.Value == authModelId {
+					foundAuthModelId = true
+				}
+			}
+
+			if !foundStoreId {
+				return fmt.Errorf("container %s does not have env var %s with value %s", container.Name, extensionsv1.OpenFgaStoreIdEnv, storeId)
+			}
+			if !foundAuthModelId {
+				return fmt.Errorf("container %s does not have env var %s with value %s", container.Name, extensionsv1.OpenFgaAuthModelIdEnv, authModelId)
+			}
+		}
+
+		// Validate that the deployment has the annotation OpenFgaAuthModel with the correct version
+		if deployment.Annotations[extensionsv1.OpenFgaAuthModelVersionLabel] != modelVersion.String() {
+			return fmt.Errorf("deployment does not have annotation %s with value %s", extensionsv1.OpenFgaAuthModelVersionLabel, modelVersion.String())
+		}
+
+		return nil
+	}, duration, interval).Should(Succeed())
+}
